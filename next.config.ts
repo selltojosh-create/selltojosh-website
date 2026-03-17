8c4f19efb87c02489228059c56ec5c14dbc8d43a
Josh Isbell <josh.isbell@isbellrealtors.com>
2026-03-16 23:51:28 -0500

    feat: add Google Tag Manager with CSP support
    - GTM script in head + noscript iframe after body open
    - Conditional on NEXT_PUBLIC_GTM_ID env var
    - CSP updated to allow googletagmanager.com and google-analytics.com

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const cspDirectives = [
   "default-src 'self'",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://va.vercel-scripts.com",
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://www.googletagmanager.com https://va.vercel-scripts.com",
   "style-src 'self' 'unsafe-inline'",
-  "img-src 'self' data: https:",
-  "font-src 'self'",
-  "connect-src 'self' https://www.google.com https://vitals.vercel-insights.com https://*.sanity.io",
-  "frame-src https://www.google.com https://www.youtube.com",
+  "img-src 'self' data: https: https://www.googletagmanager.com https://www.google-analytics.com",
+  "font-src 'self' https://fonts.gstatic.com",
+  "connect-src 'self' https://www.google.com https://www.google-analytics.com https://www.googletagmanager.com https://vitals.vercel-insights.com https://*.sanity.io",
+  "frame-src https://www.google.com https://www.youtube.com https://www.googletagmanager.com",
   "media-src 'self' https:",
 ];
 
